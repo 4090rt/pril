689061f79366fbee8cf6b52a084c673dd5fbb6e7
4090rt <artem2007yannurow@gmail.com>
2025-06-05 18:15:43 +0500

    Add files via upload

---
+++ reg — копия/reg/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reg
+{
+    class Class1
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+        public void usersss(string name, string password)
+        {
+            Name = name;
+            Password = password;
+        }
+    }
+}
---
+++ reg — копия/reg/Form2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.VisualBasic.Logging;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace reg
+{
+    public partial class Form2 : Form
+    {
+        private Form1 mainForm;
+
+        public Form2(Form1 form)
+        {
+            InitializeComponent();
+            mainForm = form;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            string password = textBox2.Text;
+            string email = textBox3.Text;
+            //mainForm.AuthorizeUser(name, password, email);
+            Form3 form3 = new Form3();
+            form3.Show();
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.Show();
+            this.Close();
+        }
+    }
+}
